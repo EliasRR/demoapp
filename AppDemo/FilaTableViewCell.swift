d1a446782b45dbfef1cc1b5c9349fd58efffa2b8
ADMINISTRADORUTM <ADMINUTM@iMac-de-TecMilenio.local>
2016-12-06 09:57:07 -0600

    Actualizado 10:00 am 6 dic

---
+++ AppDemo/FilaTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FilaTableViewCell.swift
+//  AppDemo
+//
+//  Created by adminisitradorUTM on 05/12/16.
+//  Copyright © 2016 cursodeios. All rights reserved.
+//
+
+import UIKit
+
+class FilaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbIzquierda: UILabel!
+    @IBOutlet weak var lbDerecha: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- AppDemo/ViewController.swift
+++ AppDemo/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var array = ["Hola"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+       return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        /*let proto = (indexPath.row % 2 == 0) ? "proto1" : "proto2"
+        
+        let vista = tableView.dequeueReusableCell(withIdentifier: proto, for: indexPath) as! FilaTableViewCell
+        
+        if(indexPath.row % 2 == 0){
+            vista.lbIzquierda.text = "Index!"
+            vista.lbDerecha.text = "\(indexPath.row)"
+        }else
+        {
+            vista.lbDerecha.text = "Index!"
+            vista.lbIzquierda.text = "\(indexPath.row)"
+        }
+        
+        return vista*/
+        
+        //F2
+        
+        let vista = tableView.dequeueReusableCell(withIdentifier: "proto1", for: indexPath) as! FilaTableViewCell
+        
+            vista.lbIzquierda.text = "Index!"
+            vista.lbDerecha.text = "\(indexPath.row)"
+        
+        return vista
+        
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath:  IndexPath)    {
+    print("Fila\(indexPath.row)" )
+    }
+    
+    @IBOutlet weak var entrar: UIButton!
 
 }
 
